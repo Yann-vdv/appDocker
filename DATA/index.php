b9a45727491999ed0aaa47884b984fd9ecbbe6c7
Yann-vdv <61906860+Yann-vdv@users.noreply.github.com>
2023-07-12 21:20:15 +0200

    fix

--- DATA/index.php
+++ DATA/index.php
@@ -6,43 +6,26 @@ echo "Connected to MySQL<br />";
 mysqli_close($connection);
 ?>
 <?php
-//phpinfo()
 
-function CallAPI($method, $url, $data = false)
-{
+    $url = 'http://api:3000/api/recette/client';
+
     $token = "#£kc+[¨uuayeizrrtui&/&mdf4£@¤a&-";
-    $curl = curl_init();
-    $authorization = 'Authorization: Bearer'.$token;
-    switch ($method)
-    {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
 
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data)
-                $url = sprintf("%s?%s", $url, http_build_query($data));
+    $options = [
+        'http' => [
+            'method' => 'GET',
+            'header' => 'Content-Type: application/json' . "\r\n" .
+                    'Authorization: Bearer ' . $token
+        ],
+    ];
+
+    $context = stream_context_create($options);
+    $response = file_get_contents($url, true, $context);
+
+    if ($response === false) {
+        echo('error2' . $response);
+    } else {
+        // Process the response
+        echo $response;
     }
-
-    // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $result = curl_exec($curl);
-    if (curl_errno($curl)) {
-        $error_msg = curl_error($curl);
-        var_dump($error_msg);
-    }
-    curl_close($curl);
-
-    return $result;
-}
-var_dump(CallAPI("GET","http://localhost:3005/api/recette/client"))
 ?>
-
